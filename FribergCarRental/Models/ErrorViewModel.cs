cc3f75b69671d4dc8195d3410b8d9e45cbdd32a9
xergon <christoffer.isenberg@gmail.com>
2025-01-10 12:57:00 +0100

    Add project files.

---
+++ FribergCarRental/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FribergCarRental.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ FribergCarRental/data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FribergCarRental.data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
