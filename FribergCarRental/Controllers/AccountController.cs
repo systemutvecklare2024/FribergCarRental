b5d670fe9923eeb489ed6819588af2a02f3524d1
xergon <christoffer.isenberg@gmail.com>
2025-01-13 13:59:30 +0100

    AccountController

---
+++ FribergCarRental/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FribergCarRental.Models;
+using FribergCarRental.Services;
+
+namespace FribergCarRental.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AccountController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+
+        // GET: Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginViewModel loginViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_authService.Login(loginViewModel.Account, loginViewModel.Password))
+                {
+                    return RedirectToAction("Secret");
+                } else
+                {
+                    ModelState.AddModelError("", "Invalid account or password.");
+                }
+            }
+            //return View(user);
+            return View(loginViewModel);
+        }
+
+
+        public IActionResult Secret()
+        {
+            HttpContextAccessor asd = new HttpContextAccessor();
+            var user = asd?.HttpContext?.Session.GetString("User");
+            ViewBag.User = user;
+            return View();
+        }
+    }
+
+}
---
+++ FribergCarRental/Models/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FribergCarRental.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Account { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
