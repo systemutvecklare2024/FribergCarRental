1704f74a87a39eec4ad8a9f7d80506980f835020
xergon <christoffer.isenberg@gmail.com>
2025-01-14 15:57:41 +0100

    Added Admin view

---
+++ FribergCarRental/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using FribergCarRental.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FribergCarRental.Areas.Admin.Controllers
+{
+    [SimpleAuthorize( Role = "Admin")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
