55dfa29711aeb1f95fe4b216680d531a8e6a50a3
xergon <christoffer.isenberg@gmail.com>
2025-02-07 21:44:45 +0100

    Updated admin panel

--- FribergCarRental/Areas/Admin/Controllers/AdminController.cs
+++ FribergCarRental/Areas/Admin/Controllers/AdminController.cs
@@ -1,16 +1,56 @@
-﻿using FribergCarRental.Filters;
+﻿using FribergCarRental.Areas.Admin.Models;
+using FribergCarRental.data;
+using FribergCarRental.Filters;
+using FribergCarRental.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FribergCarRental.Areas.Admin.Controllers
 {
-    [SimpleAuthorize( Role = "Admin")]
     [Area("Admin")]
     public class AdminController : Controller
     {
-        //GET: Admin
-        public IActionResult Index()
+        private readonly IUserRepository _userRepository;
+        private readonly IBookingRepository _bookingRepository;
+        private readonly ICarRepository _carRepository;
+        private readonly IAuthService _authService;
+
+        public AdminController(IUserRepository userRepository, IBookingRepository bookingRepository, ICarRepository carRepository, IAuthService authService)
         {
-            return View();
+            _userRepository = userRepository;
+            _bookingRepository = bookingRepository;
+            _carRepository = carRepository;
+            _authService = authService;
+        }
+        //GET: Admin
+        public async Task<IActionResult> Index()
+        {
+            if (!_authService.IsAuthenticated() && !await _authService.IsAdmin())
+            {
+                return RedirectToAction("LoginOrRegister", "Account", new { 
+                    area = "",
+                    returnUrl = Url.Action("Index", "Admin", new { area = "Admin"} ) });
+            }
+
+            return RedirectToAction("Dashboard", "Admin");
+            
+        }
+
+        // Get: Admin/Dashboard
+        [SimpleAuthorize( Role = "Admin")]
+        public async Task<IActionResult> Dashboard()
+        {
+            var users = await _userRepository.AllAsync() ?? [];
+            var cars = await _carRepository.AllAsync() ?? [];
+            var bookings = await _bookingRepository.AllAsync() ?? [];
+
+            var dashboard = new AdminDashboardViewModel()
+            {
+                BookingsCount = bookings.Count(),
+                CarsCount = cars.Count(),
+                UsersCount = users.Count(),
+            };
+
+            return View(dashboard);
         }
     }
 }
---
+++ FribergCarRental/Areas/Admin/Models/AdminDashboardViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FribergCarRental.Areas.Admin.Models
+{
+    public class AdminDashboardViewModel
+    {
+        public int BookingsCount { get; set; }
+        public int CarsCount { get; set; }
+        public int UsersCount { get; set; }
+    }
+}
