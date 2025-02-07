37d66104b6ed67c2290b362ee36680782eb02656
xergon <christoffer.isenberg@gmail.com>
2025-02-07 21:48:18 +0100

    Added ViewComponents for UserForm and ContactForm

---
+++ FribergCarRental/ViewComponents/ContactFormViewComponent.cs
@@ -0,0 +1,46 @@
+﻿using FribergCarRental.Models.ViewModel;
+using FribergCarRental.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FribergCarRental.ViewComponents
+{
+    public class ContactFormViewComponent : ViewComponent
+    {
+        private readonly IAuthService _authService;
+
+        public ContactFormViewComponent(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _authService.GetAccount();
+            if (user == null)
+            {
+                ViewBag.Warning = "Vi kunde inte ladda dina uppgifter.";
+                return View(new UserViewModel());
+            }
+
+            var contact = user.Contact;
+            if (contact == null)
+            {
+                ViewBag.Warning = "Vi kunde inte ladda dina uppgifter.";
+                return View(new UserViewModel());
+            }
+
+            var contactViewModel = new ContactViewModel
+            {
+                Id = contact.Id,
+                FirstName = contact.FirstName,
+                LastName = contact.LastName,
+                Address = contact.Address,
+                City = contact.City,
+                PostalCode = contact.PostalCode,
+                Phone = contact.Phone
+            };
+
+            return View(contactViewModel);
+        }
+    }
+}
---
+++ FribergCarRental/ViewComponents/UserFormViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using FribergCarRental.Models.ViewModel;
+using FribergCarRental.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FribergCarRental.ViewComponents
+{
+    public class UserFormViewComponent : ViewComponent
+    {
+        private readonly IAuthService _authService;
+
+        public UserFormViewComponent(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _authService.GetCurrentUser();
+            if (user == null)
+            {
+                ViewBag.Warning = "Vi kunde inte ladda dina uppgifter.";
+                return View(new UserViewModel());
+            }
+
+            var model = new UserViewModel
+            {
+                Id = user.Id,
+                Email = user.Email,
+                Username = user.Username
+            };
+
+            return View(model);
+        }
+    }
+}
